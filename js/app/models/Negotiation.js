70d99dce6b48ae5be043d2b0cf214310eae1722d
Antonio Moraes <antonio_moraes@econeteditora.com.br>
2020-12-08 17:07:55 -0300

    Lesson 02-08 - Defensive Programming

--- js/app/models/Negotiation.js
+++ js/app/models/Negotiation.js
@@ -1,14 +1,14 @@
 class Negotiation {
 
     constructor(date, amount, value) {
-        this._date = date;
+        this._date = new Date(date.getTime());
         this._amount = amount;
         this._value = value;
         this._volume = amount * value;
         Object.freeze(this);
     }
 
-    get date() { return this._date; }
+    get date() { return new Date(this._date.getTime()); }
     get amount() { return this._amount; }
     get value() { return this._value; }
     get volume() { return this._volume; }
